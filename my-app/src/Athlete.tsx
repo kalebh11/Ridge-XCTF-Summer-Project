368eb2ee193184c86b11da8c9bbd676a493ef5c5
Kaleb Huang <kalebhuang@Kalebs-MacBook-Air.local>
2023-06-13 15:20:03 -0400

    save

---
+++ my-app/src/Athlete.tsx
@@ -0,0 +1,18 @@
+class Athlete{
+
+    first_name : string;
+    last_name : string;
+    grade : number;
+    group : string;
+    id : string;
+
+    constructor(fname : string, lname: string, gradeN : number, groupN : string){
+       this.first_name = fname;
+       this.last_name = lname;
+       this.grade = gradeN;
+       this.group = groupN;
+       this.id = "test"; //change later
+    }
+
+   
+}
