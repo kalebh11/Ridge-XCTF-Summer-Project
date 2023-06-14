2265141414dd7eb2ca9f8d2389140dc306017dcd
Kaleb Huang <kalebhuang@Kalebs-MacBook-Air.local>
2023-06-14 09:29:50 -0400

    save

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,10 +3,13 @@ import React from "react";
 const InputField = () => {
     return(
     <div>
-      <button type = "button" className = "button">Roster</button>
-      <button className = "button" style = {{verticalAlign:"middle"}}><span>Meet Lineup</span></button>
+      <button type = "button" className = "button"><span>Roster</span></button>;
+      <button className = "button1" style = {{verticalAlign:"middle"}}><span>Meet Lineup</span></button>;
+      <button className = "button2"> <span>Meet Results</span></button>;
+      <button className = "button3"><span> Calender </span></button>;
+      <img src= "./images/ridgelogo.png"/>;
     </div>
-    )
+    ) 
 }
 
 export default InputField;
