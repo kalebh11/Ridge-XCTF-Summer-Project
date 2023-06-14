56c9e449d0958022d573e8945f0fa8ddf088bd4c
kalebh11 <kalebhuang18@gmail.com>
2023-06-13 21:34:02 -0400

    andrew is a fatty

---
+++ src/components/InputField.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InputField = () => {
+    return(
+    <div>
+      <button type = "button" className = "button">Roster</button>
+      <button className = "button" style = {{verticalAlign:"middle"}}><span>Meet Lineup</span></button>
+    </div>
+    )
+}
+
+export default InputField;
