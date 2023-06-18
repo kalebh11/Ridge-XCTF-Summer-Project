b2af787d695ca0ea458ed22cad4cf5286e54bd44
Kaleb Huang <kalebhuang@Kalebs-MacBook-Air.local>
2023-06-18 17:39:52 -0400

    save

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="top-bar">
+    <div className="outer">
       <div className="header">
         <span>Ridge XCTF</span>
       </div>
