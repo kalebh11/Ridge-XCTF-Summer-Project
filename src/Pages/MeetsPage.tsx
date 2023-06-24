53c98a9ec132e0c85a37a69e0614d2bf45b19467
kalebh11 <kalebhuang18@gmail.com>
2023-06-24 09:42:38 -0400

    save

--- src/Pages/MeetsPage.tsx
+++ src/Pages/MeetsPage.tsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import { VscAdd } from "react-icons/vsc";
 const MeetsPage = () => {
-  return <div></div>;
+  return (
+    <div className="outer2">
+      <div className="meet-top-bar">
+        <div className="new-meet-button-area">
+          <button className="new-button">
+            <div className="meets-add-icon-container">
+              <VscAdd className="meets-add-icon" />
+            </div>
+
+            <div className="new-button-text">New</div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MeetsPage;
