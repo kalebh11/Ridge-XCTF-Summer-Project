ee3368ce42e79ab9dd0aee7472416dd70d1708f0
kalebh11 <kalebhuang18@gmail.com>
2023-06-24 22:53:50 -0400

    save

--- src/Pages/Meets/MeetsPopup.tsx
+++ src/Pages/Meets/MeetsPopup.tsx
@@ -19,6 +19,10 @@ const MeetsPopup: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
     onClose(); // Close the popup after submission
   };
 
+  const handleClose = () => {
+    onClose();
+  };
+
   return (
     <Modal
       isOpen={isOpen}
@@ -27,13 +31,34 @@ const MeetsPopup: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
     >
       <form onSubmit={handleSubmit} className="meets-popup-form">
         <div className="meets-popup-top">
+          <span>Enter Meet Information</span>
           <div className="meets-popup-x-area">
-            <GrClose />
+            <span className="meets-popup-x">
+              <button onClick={handleClose}>
+                <GrClose className="meets-add-icon" />
+              </button>
+            </span>
           </div>
         </div>
         <div className="meets-popup-mid">
           <label>
             Meet Name
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Meet Location
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Meet Date
             <input
               type="text"
               value={inputValue}
