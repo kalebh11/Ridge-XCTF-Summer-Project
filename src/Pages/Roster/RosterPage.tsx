6cd1faf0fdb8ab45e1260e78a1391475822ae9cd
Kaleb Huang <kalebhuang@Kalebs-MacBook-Air.local>
2023-07-05 14:24:38 -0400

    save

--- src/Pages/Roster/RosterPage.tsx
+++ src/Pages/Roster/RosterPage.tsx
@@ -1,21 +1,73 @@
-import React from "react";
+import React, { ChangeEventHandler, useState } from "react";
 import "./Roster.scss";
 import "bootstrap/dist/js/bootstrap";
+import TableComponent from "./TableComponent";
 
-interface Athlete {
+export interface Athlete {
   name: string;
   grade: number;
   group: string;
   id: string;
 }
 
-interface DistanceAthlete extends Athlete {
+export interface DistanceAthlete extends Athlete {
+  map(
+    arg0: (
+      athlete: DistanceAthlete,
+      index: React.Key | null | undefined
+    ) => import("react/jsx-runtime").JSX.Element
+  ): React.ReactNode;
   vdot: number;
 }
 
 const RosterPage = () => {
-  const handleSubmitAthlete = () => {
-    console.log("sent");
+  const [athleteList, setAthletesList] = useState<Athlete[]>([]);
+  const [name, setName] = useState("");
+  const [grade, setGrade] = useState(0);
+  const [group, setGroup] = useState("");
+  const [vdot, setVdot] = useState(0);
+  const id = "";
+
+  const handleNameChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setName(e.target.value);
+  };
+  const handleGradeChange = (e: { target: { value: any } }) => {
+    setGrade(Number(e.target.value));
+  };
+  const handleGroupChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setGroup(e.target.value);
+  };
+  const handleVdotChange = (e: { target: { value: any } }) => {
+    setVdot(Number(e.target.value));
+  };
+  const generateRandomID = (): string => {
+    const array = new Uint32Array(1);
+    crypto.getRandomValues(array);
+    return array[0].toString(36);
+  };
+
+  const handleSubmitAthlete = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const newAthlete: DistanceAthlete = {
+      name,
+      group,
+      grade,
+      vdot,
+      id: generateRandomID(),
+    };
+
+    setAthletesList([...athleteList, newAthlete]);
+
+    // Reset the input values
+    setName("");
+    setGroup("");
+    setGrade(0);
+    setVdot(0);
   };
 
   return (
@@ -73,10 +125,75 @@ const RosterPage = () => {
             aria-labelledby="nav-home-tab"
           >
             <div className="roster-nav-main-container">
-              <div className="roster-table-container">distance</div>
+              <div className="roster-table-container">
+                <TableComponent athleteList={athleteList} />
+              </div>
               <div className="roster-form-container">
                 <div className="roster-form-main-container">
-                  <input type="number"></input>
+                  <div className="roster-form-input-container">
+                    <div className="form__group field">
+                      <input
+                        type="input"
+                        className="form__field"
+                        placeholder="Name"
+                        name="name"
+                        id="name"
+                        required
+                        onChange={handleNameChange}
+                      />
+                      <label htmlFor="name" className="form__label">
+                        Athlete Name
+                      </label>
+                    </div>
+                  </div>
+                  <div className="roster-form-input-container">
+                    <div className="form__group field">
+                      <input
+                        type="number"
+                        className="form__field"
+                        placeholder="Grade"
+                        name="grade"
+                        id="grade"
+                        required
+                        onChange={handleGradeChange}
+                      />
+                      <label htmlFor="grade" className="form__label">
+                        Athlete Grade
+                      </label>
+                    </div>
+                  </div>
+                  <div className="roster-form-input-container">
+                    <div className="form__group field">
+                      <input
+                        type="input"
+                        className="form__field"
+                        placeholder="Group"
+                        name="group"
+                        id="group"
+                        required
+                        onChange={handleGroupChange}
+                      />
+                      <label htmlFor="group" className="form__label">
+                        Athlete Group
+                      </label>
+                    </div>
+                  </div>
+                  <div className="roster-form-input-container">
+                    <div className="form__group field">
+                      <input
+                        type="number"
+                        className="form__field"
+                        placeholder="Vdot"
+                        name="vdot"
+                        id="vdot"
+                        required
+                        onChange={handleVdotChange}
+                      />
+                      <label htmlFor="vdot" className="form__label">
+                        Athlete VDOT
+                      </label>
+                    </div>
+                  </div>
                 </div>
                 <div className="roster-form-submit-container">
                   <button
---
+++ src/Pages/Roster/TableComponent.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { DistanceAthlete } from "./RosterPage";
+
+interface Props {
+  athleteList: DistanceAthlete;
+}
+const TableComponent: React.FC<Props> = ({ athleteList }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Group</th>
+          <th>Grade</th>
+          <th>VDOT</th>
+        </tr>
+      </thead>
+      <tbody>
+        {athleteList.map((item) => (
+          <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>{item.group}</td>
+            <td>{item.grade}</td>
+            <td>{item.vdot}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TableComponent;
