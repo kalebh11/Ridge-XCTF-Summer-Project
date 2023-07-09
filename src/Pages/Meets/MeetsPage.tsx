4c5f1e217d294f5724ff852085887eb0035ff4ea
kalebh11 <kalebhuang18@gmail.com>
2023-07-09 16:38:40 -0400

    save

--- src/Pages/Meets/MeetsPage.tsx
+++ src/Pages/Meets/MeetsPage.tsx
@@ -2,9 +2,25 @@ import React, { useState } from "react";
 import { VscAdd } from "react-icons/vsc";
 import MeetsPopup, { MeetObject } from "./MeetsPopup";
 import MeetCard from "./MeetComponents/MeetCard";
+import firebase, { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0ao2imRtQ-hi2kc6XsugIGfc1aXTA7g0",
+  authDomain: "track-app-49793.firebaseapp.com",
+  projectId: "track-app-49793",
+  storageBucket: "track-app-49793.appspot.com",
+  messagingSenderId: "713239875011",
+  appId: "1:713239875011:web:04f51cf11b3b2eafff4176",
+  measurementId: "G-YT820FDWF5",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
 const MeetsPage = () => {
   let [isPopupOpen, setIsPopupOpen] = useState(false);
-  let [meetObject, setMeetObject] = useState<MeetObject | null>(null);
   let [meetList, setMeetList] = useState<MeetObject[]>([]);
 
   const openPopup = () => {
@@ -16,7 +32,6 @@ const MeetsPage = () => {
   };
 
   const handleFormSubmit = (meetObject: MeetObject) => {
-    setMeetObject(meetObject);
     setMeetList((prevList) => [...prevList, meetObject]);
   };
 
@@ -31,14 +46,18 @@ const MeetsPage = () => {
             </div>
             <div className="new-button-text">New</div>
           </button>
-          <MeetsPopup isOpen={isPopupOpen} onClose={closePopup} onSubmit={handleFormSubmit} />
+          <MeetsPopup
+            isOpen={isPopupOpen}
+            onClose={closePopup}
+            onSubmit={handleFormSubmit}
+          />
         </div>
       </div>
       <div className="meets-mid-section">
         <div className="meet-cards-container">
-            {meetList.map((item) => (
-              <MeetCard key={item.id} object={item} />
-            ))}
+          {meetList.map((item) => (
+            <MeetCard key={item.id} object={item} />
+          ))}
         </div>
       </div>
     </div>
